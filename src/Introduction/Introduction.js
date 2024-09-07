ab2280449644d599f376b3eabf323bb47a2ac623
HosseinDev-02 <hosseinDev2002@gmail.com>
2024-09-07 20:34:12 +0330

    finish codding introduction section

--- src/Introduction/Introduction.js
+++ src/Introduction/Introduction.js
@@ -3,27 +3,29 @@ export default function Introduction () {
     return (
         <section className='py-5'>
             <div className='container'>
-                <div className='bg-gradient-to-l from-zinc-100 to-white rounded-2xl flex items-center justify-center py-16 gap-10'>
+                <div className='bg-gradient-to-l from-zinc-100 dark:from-mirage to-white dark:to-black rounded-2xl flex flex-col-reverse xl:flex-row items-center justify-center py-16 gap-10'>
                     {/*  introduction right side  */}
                     <div className='max-w-[620px] space-y-5'>
                         {/*  Introduction right side offer  */}
                         <div className='flex items-center gap-2'>
-                            <div className='flex items-center gap-1 bg-primary py-1 px-2 rounded-xl text-white'>
+                            <div className='flex items-center gap-1 bg-primary py-1 px-2 rounded-xl text-white animate-pulse transition-all'>
                                 <span>
                                     <svg className='w-4 h-4'>
                                         <use href='#receipt-percent'></use>
                                     </svg>
                                 </span>
-                                <span className='text-xs font-semibold'>جشنواره تخفیف !</span>
+                                <span className='text-xs font-semibold'>جشنــــواره تخفیف !</span>
                             </div>
                             <span className='text-xs text-primary font-semibold'>
                                 به زودی :)
                             </span>
                         </div>
-                        <h1 className='font-black text-5xl text-title font-YekanBakh-ExtraBold max-w-[485px]'>
-                            داستان برنامه نویس شدنت از اینجا شروع میشه !
+                        <h1 className='font-black text-4xl md:text-5xl text-title dark:text-white font-YekanBakh-ExtraBold'>
+                            داستان برنامه نویس شدنت
+                            <br/>
+                             از اینجا شروع میشه !
                         </h1>
-                        <p className='tracking-wider font-YekanBakh-Medium'>
+                        <p className='tracking-wider font-YekanBakh-Medium max-w-80 xl:max-w-none'>
                             یادگیری برنامه‌نویسی آرزو نیست، فقط نیاز هست که تلاش و تمرین داشته
                             باشید، بقیه‌اش با نابغه
                         </p>
@@ -36,7 +38,7 @@ export default function Introduction () {
                             </span>
                         </a>
                     </div>
-                    <div className='w-72'>
+                    <div className='max-w-72'>
                         <img className='w-full h-full object-cover' src="./images/Introduction/header-man.png" alt=""/>
                     </div>
                 </div>
