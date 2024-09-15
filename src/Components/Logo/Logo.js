2d9b5d7df627d76e310ba5c7a67c5ab64c9fbd50
HosseinDev-02 <hosseinDev2002@gmail.com>
2024-09-15 19:14:49 +0330

    refactor Logo component 2

--- src/Components/Logo/Logo.js
+++ src/Components/Logo/Logo.js
@@ -4,7 +4,7 @@ export default function Logo() {
                 <use href='#logo'></use>
             </svg>
             <span className='flex items-start flex-col'>
-                <span className='text-sm font-YekanBakh-SemiBold'>سیکتیرا</span>
+                <span className='text-sm font-YekanBakh-SemiBold'>آکــــادمـــی</span>
                 <span className='text-primary text-xl font-YekanBakh-Black'>نـــابــــغه</span>
             </span>
         </a>)
