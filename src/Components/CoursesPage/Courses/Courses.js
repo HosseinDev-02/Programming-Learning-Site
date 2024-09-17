c689f56e50a08bf51e8ac52bf31a38a06ad9119a
HosseinDev-02 <hosseinDev2002@gmail.com>
2024-09-17 13:50:56 +0330

    codding accordion boxes and add two state to Courses component showCategories, showTypes

--- src/Components/CoursesPage/Courses/Courses.js
+++ src/Components/CoursesPage/Courses/Courses.js
@@ -1,5 +1,32 @@
 import SectionTitle from "../../SectionTitle/SectionTitle";
+import {useState} from "react";
+
 export default function Courses() {
+
+    const [showCategories, setShowCategories] = useState(false)
+    const [showTypes, setShowTypes] = useState(false)
+
+
+    const typesMenuHandler = (e) => {
+        e.currentTarget.classList.toggle('text-primary')
+        e.currentTarget.classList.toggle('text-title')
+        if(showTypes){
+            setShowTypes(false)
+        }else {
+            setShowTypes(true)
+        }
+    }
+
+    const categoriesMenuHandler = (e) => {
+        e.currentTarget.classList.toggle('text-primary')
+        e.currentTarget.classList.toggle('text-title')
+        if(showCategories){
+            setShowCategories(false)
+        }else {
+            setShowCategories(true)
+        }
+    }
+
     return (
         <main className='py-5'>
             <div className="container">
@@ -34,8 +61,8 @@ export default function Courses() {
                             {/*  Courses Accordion boxes  */}
                             <div className='divide-y divide-border'>
                                 {/*  Courses Accordion Free Or Not Free Filtering  */}
-                                <div className='mb-3'>
-                                    <button type='button'
+                                <div className='mb-3 space-y-2'>
+                                    <button onClick={typesMenuHandler} type='button'
                                             className='text-sm font-YekanBakh-SemiBold text-title flex items-center justify-between w-full h-11 bg-secondary rounded-xl overflow-hidden px-3'>
                                         <div className='flex items-center gap-2'>
                                         <span>
@@ -53,10 +80,51 @@ export default function Courses() {
                                         </svg>
                                     </span>
                                     </button>
+                                    <div style={showTypes ? {display: 'block'} : {display: 'none'}} className='p-3 rounded-xl overflow-hidden bg-secondary space-y-2'>
+                                        <label className='flex items-center justify-between text-sm'>
+                                            <div className='flex items-center gap-3'>
+                                                <input
+                                                    className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                    type="radio" name='type'/>
+                                                <span>
+                                                    رایگان
+                                                </span>
+                                            </div>
+                                            <span>
+                                                18
+                                            </span>
+                                        </label>
+                                        <label className='flex items-center justify-between text-sm'>
+                                            <div className='flex items-center gap-3'>
+                                                <input
+                                                    className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                    type="radio" name='type'/>
+                                                <span>
+                                                    فقط نفدی
+                                                </span>
+                                            </div>
+                                            <span>
+                                                9
+                                            </span>
+                                        </label>
+                                        <label className='flex items-center justify-between text-sm'>
+                                            <div className='flex items-center gap-3'>
+                                                <input
+                                                    className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                    type="radio" name='type'/>
+                                                <span>
+                                                    نقدی و اعضای ویژه
+                                                </span>
+                                            </div>
+                                            <span>
+                                                48
+                                            </span>
+                                        </label>
+                                    </div>
                                 </div>
                                 {/*  Courses Accordion Categories Filtering  */}
-                                <div className='pt-3'>
-                                    <button type='button'
+                                <div className='pt-3 space-y-2'>
+                                    <button onClick={categoriesMenuHandler} type='button'
                                             className='text-sm font-YekanBakh-SemiBold text-title flex items-center justify-between bg-secondary rounded-xl overflow-hidden h-11 w-full px-3'>
                                         <div className='flex items-center gap-2'>
                                         <span>
@@ -74,6 +142,48 @@ export default function Courses() {
                                         </svg>
                                     </span>
                                     </button>
+                                    <div style={showCategories ? {display: 'block'} : {display: 'none'}} className='p-3 rounded-xl overflow-hidden bg-secondary space-y-2'>
+                                        <label className='flex items-center gap-3 text-sm'>
+                                            <input
+                                                className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                type="radio" name='category'/>
+                                            <span>
+                                                فرانت اند
+                                            </span>
+                                        </label>
+                                        <label className='flex items-center gap-3 text-sm'>
+                                            <input
+                                                className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                type="radio" name='category'/>
+                                            <span>
+                                                بک اند
+                                            </span>
+                                        </label>
+                                        <label className='flex items-center gap-3 text-sm'>
+                                            <input
+                                                className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                type="radio" name='category'/>
+                                            <span>
+                                                امنیت
+                                            </span>
+                                        </label>
+                                        <label className='flex items-center gap-3 text-sm'>
+                                            <input
+                                                className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                type="radio" name='category'/>
+                                            <span>
+                                                شبکه
+                                            </span>
+                                        </label>
+                                        <label className='flex items-center gap-3 text-sm'>
+                                            <input
+                                                className='bg-border w-4 h-4 appearance-none rounded-full checked:bg-transparent transition-all border-primary border-0 checked:border-[5px]'
+                                                type="radio" name='category'/>
+                                            <span>
+                                                مهارت های نرم
+                                            </span>
+                                        </label>
+                                    </div>
                                 </div>
                             </div>
                         </div>
