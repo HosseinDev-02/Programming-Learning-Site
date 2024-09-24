3f97cf3898f7fbb8c4d754b767fcc8e79f9fe510
HosseinDev-02 <hosseinDev2002@gmail.com>
2024-09-24 20:47:46 +0330

    codding menu items in Panel component sidebar

--- src/Components/Panel/Panel.js
+++ src/Components/Panel/Panel.js
@@ -3,7 +3,7 @@ import SectionTitle from "../Titles/SectionTitle";
 export default function Panel() {
 
     return (
-        <div className='flex gap-5 h-full bg-secondary'>
+        <div className='flex gap-5 bg-secondary min-h-screen'>
             {/*  Admin Panel Right Side  */}
             <div className='bg-background w-80 rounded-l-2xl p-5'>
                 <div className='flex flex-col items-center gap-2'>
@@ -16,52 +16,52 @@ export default function Panel() {
                 </div>
                 <div className='mt-8'>
                     <ul className='flex flex-col gap-2'>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 منو
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 زیر منو
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 خدمات
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 دوره ها
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 دسته بندی ها
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 مدرس ها
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 سبد خرید
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 کاربران
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 کامنت ها
                             </span>
                         </li>
-                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-10 cursor-pointer'>
+                        <li className='flex items-center gap-2 bg-secondary hover:bg-primary transition-colors hover:text-white rounded-xl px-4 h-11 cursor-pointer font-YekanBakh-SemiBold text-title'>
                             <span>
                                 مقاله ها
                             </span>
